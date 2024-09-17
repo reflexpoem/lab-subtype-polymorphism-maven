58c7d4793264479f9179ef1106394bb2beca7991
Sunjae_Kim <kimsunja@grinnell.edu>
2024-09-17 15:06:00 -0500

    updated Blocks

--- src/main/java/edu/grinnell/csc207/experiments/Blocks.java
+++ src/main/java/edu/grinnell/csc207/experiments/Blocks.java
@@ -4,6 +4,7 @@ import java.io.PrintWriter;
 
 import edu.grinnell.csc207.blocks.AsciiBlock;
 import edu.grinnell.csc207.blocks.Boxed;
+import edu.grinnell.csc207.blocks.Empty;
 import edu.grinnell.csc207.blocks.Line;
 import edu.grinnell.csc207.blocks.Rectangle;
 
@@ -51,6 +52,32 @@ public class Blocks {
     AsciiBlock b4 = new Boxed(b2);
     AsciiBlock.print(pen, b4);
 
+    separator(pen);
+    pen.printf("b5 = new Boxed(b5)\n\n");
+    AsciiBlock b5 = new Boxed(b4);
+    AsciiBlock.print(pen, b5);
+
+    separator(pen);
+    pen.printf("b6 = new Boxed(b5)\n\n");
+    AsciiBlock b6 = new Boxed(b5);
+    AsciiBlock.print(pen, b6);
+
+    separator(pen);
+    pen.printf("b7 = new Empty()\n\n");
+    AsciiBlock b7 = new Empty();
+    AsciiBlock.print(pen, b7);
+
+    separator(pen);
+    pen.printf("b8 = new Boxed(b8)\n\n");
+    AsciiBlock b8 = new Boxed(b7);
+    AsciiBlock.print(pen, b8);
+
+    separator(pen);
+    pen.printf("b9 = new Boxed(b8)\n\n");
+    AsciiBlock b9 = new Boxed(b8);
+    AsciiBlock.print(pen, b9);
+
+    
     pen.close();
   } // main(String[])
 } // class Blocks
